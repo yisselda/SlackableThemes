98ca74f909682593c5f2a1ae20bb69a793642ed9
Jc7j <chiangjason19@gmail.com>
2019-10-05 16:52:18 -0700

    Added WorkspaceSideBar component to PageContainer

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import SideBar from '../components/SideBar';
 import DashBoard from '../components/DashBoard';
+import WorkspaceSideBar from '../components/WorkspaceSideBar';
+
+import styles from "../styles/PageContainer.modules.css";
+
 
 const PageContainer = props => {
   return (
-    <div>
+    <div className="pageContainer">
+      <WorkspaceSideBar/>
       <SideBar/>
       <DashBoard/>
     </div>
