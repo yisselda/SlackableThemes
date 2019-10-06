0d2517c8902be82f2d531e26b227168348b2301b
Eshno <oliver-felix@hotmail.com>
2019-10-06 09:24:39 -0400

    Added ColoPicker container

--- src/components/ColorPicker/index.js
+++ src/components/ColorPicker/index.js
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
-import { SketchPicker } from 'react-color';
-import './ColorPicker.css'
+import { ChromePicker } from 'react-color';
+import './CustomPicker';
+import './ColorPicker.css';
 
-
-export default function ColorPicker(hex) {
+export default function ColorPicker({ label }) {
     let [color, setColor] = useState('#000');
+    let [shouldDisplay, setDisplay] = useState(false);
 
     const onColorChange = (e) => {
-        let newValue = e.target.value;
-        if (color.length === 7 && newValue.length >= 6) return;
+        const newValue = e.target.value;
+        console.log(newValue)
+        if (newValue.length > 7) return;
         if (newValue.length <= 1) { setColor('#'); return; };
-        if (isValid(newValue[newValue.length - 1])) setColor(newValue);
+        if (isValidHex(newValue[newValue.length - 1])) setColor(newValue);
     }
 
-    const isValid = (str) => {
+    const onColorPickChange = (newColor) => {
+        setColor(newColor.hex);
+    }
+
+    const isValidHex = (str) => {
         return !!str.match(/[A-F|a-f|0-9]/g);
     }
 
+    const handleDisplay = () => {
+        setDisplay(!shouldDisplay)
+    }
+
     return <div className="container">
-        <span className="label">Color Picker</span>
+        <span className="label">{label}</span>
         <div className="content">
-            <div className="color-sq" style={{ backgroundColor: color }}></div>
+            <div className="color-sq" onClick={handleDisplay} style={{ backgroundColor: color }}></div>
             <input className="color-input" type="text" value={color} onChange={onColorChange} />
         </div>
+        {shouldDisplay && <ChromePicker className='color-picker' color={color} onChange={onColorPickChange} />}
+        {shouldDisplay && <div className="click-outside-area" onClick={() => handleDisplay()}/>}
     </div>
 }
