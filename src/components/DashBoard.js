c9d73c85644e5717e55144bb8142f041a2237b85
Eshno <oliver-felix@hotmail.com>
2019-10-07 21:53:21 -0400

    Updated Dashboard with colorpicker

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/dashboard.scss';
 import PresetThemes from './PresetThemes';
 import ImageUpload from './ImageUpload';
@@ -6,11 +6,17 @@ import ColorPicker from './ColorPicker';
 import ColorInput from './ColorInput';
 
 const DashBoard = () => {
-  return(
+  let [color, setColor] = useState('#00ffff');
+
+  function onColorChange(newColor) {
+    setColor(newColor);
+  }
+
+  return (
     <div className='dashboard'>
       <PresetThemes />
       <ImageUpload />
-      <ColorPicker />
+      <ColorPicker color={color} onChange={onColorChange} />
       <ColorInput />
     </div>
   );
