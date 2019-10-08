f0401fffe633e398f4b7dcc3088296d6d494d497
Eshno <oliver-felix@hotmail.com>
2019-10-07 21:50:06 -0400

    Renamed & Moved ColorPicker Files

--- src/components/ColorPickers.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/color-pickers.scss';
-
-const ColorPickers = () => (
-  <div className='color-pickers'>Color Pickers</div>
-);
-
-export default ColorPickers;
