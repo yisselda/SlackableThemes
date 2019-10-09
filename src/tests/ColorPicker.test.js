d36f7318722f01d2d7fd2dc2fcd4f3fb4d253765
Eshno <oliver-felix@hotmail.com>
2019-10-08 22:16:18 -0400

    Added Tests for ColorPicker

--- src/tests/ColorPicker.test.js
+++ src/tests/ColorPicker.test.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
+import ColorPicker from '../components/ColorPicker';
 
 
-describe('ColorPicker', () => {
+describe('<ColorPicker />', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<ColorPicker/>);
+    wrapper = shallow(<ColorPicker label="Color Picker"/>);
   })
 
-  it('render <ColorPicker>', () => {
+  it('renders properly', () => {
     expect(wrapper).toBeDefined();
+  })
+
+  it('renders with the correct label', () => {
+    expect(wrapper.contains(<span className="label">Color Picker</span>)).toEqual(true);
   })
 });
