b64d9345047e9f75826fb3cdcd595e856eb6368c
Christine Xiang <christine@defmethod.io>
2019-11-21 11:38:56 -0500

    Standardize file names and fix scss styles

--- src/components/ColorInput.js
+++ src/components/ColorInput.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../styles/color-input.scss';
 
-const ColorInput = () => (
-  <div className='color-input'>Color Input</div>
-);
-
-export default ColorInput;
+export default function ColorInput(){
+  return (
+    <div className='color-input'>
+      <h2 className='color-input-title'>Copy and paste these values to share your custom theme with others</h2>
+    </div>
+  )
+};
