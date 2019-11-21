e9940f36747113919308eb2160bfe0f67ac7ddf8
Christine Xiang <christine@defmethod.io>
2019-11-21 11:09:39 -0500

    Refactor for consistency

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -5,12 +5,12 @@ import presetThemes from '../data/preset-themes';
 import '../styles/page-container.scss';
 import PropTypes from 'prop-types';
 
-const PageContainer = () => {
+export default function PageContainer(){
   const [theme, setTheme] = useState(presetThemes[0].themeColor);
 
   const updateTheme = (t) => {
     setTheme([...t]);
-  }
+  };
 
   return (
     <div className='page-container'>
@@ -18,14 +18,12 @@ const PageContainer = () => {
       <DashBoard setTheme={updateTheme} theme={theme}/>
     </div>
   )
-}
+};
 
 PageContainer.defaultProps = {
   theme: presetThemes[0].themeColor,
-}
+};
 
 PageContainer.propTypes = {
   theme: PropTypes.array,
-}
-
-export default PageContainer;
+};
