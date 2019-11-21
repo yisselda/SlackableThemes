51287bc5ef3d54dc201e986326757c114eb84306
Christine Xiang <christine@defmethod.io>
2019-11-21 11:45:40 -0500

    Render color-input with the values from the current theme

--- src/components/ColorInput.js
+++ src/components/ColorInput.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/color-input.scss';
 
-export default function ColorInput(){
+export default function ColorInput({ theme }){
   return (
     <div className='color-input'>
       <h2 className='color-input-title'>Copy and paste these values to share your custom theme with others</h2>
+      <form>
+        <input type="text" name="color-input-box" value={theme} readOnly/>
+        <input type="submit" value="Copy" />
+      </form>
     </div>
   )
 };
