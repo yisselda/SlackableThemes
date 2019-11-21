93d47e5aa9845673b2eab205997d89fa34f89b82
Christine Xiang <christine@defmethod.io>
2019-11-21 12:22:35 -0500

    Add styling for color-input

--- src/components/ColorInput.js
+++ src/components/ColorInput.js
@@ -6,8 +6,8 @@ export default function ColorInput({ theme }){
     <div className='color-input'>
       <h2 className='color-input-title'>Copy and paste these values to share your custom theme with others</h2>
       <form>
-        <input type="text" name="color-input-box" value={theme} readOnly/>
-        <input type="submit" value="Copy" />
+        <input type="text" className="color-input-box" value={theme} readOnly/>
+        <input type="submit" className="copy-button" value="Copy" />
       </form>
     </div>
   )
