59a68225cf013e36db3ea0dc045c99c6cac5d43e
Christine Xiang <christine@defmethod.io>
2020-04-17 17:18:14 -0400

    Reformat sidebar to be consistent with new slack ui

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,14 +2,13 @@
 import React from 'react';
 import '../styles/sidebar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell as farBell } from '@fortawesome/fontawesome-free-regular';
-import { faPlusCircle, faCommentDots, faChevronDown, faCaretSquareLeft, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faBookmark, faAddressCard, faEdit } from '@fortawesome/fontawesome-free-regular';
+import { faCommentDots, faChevronDown, faAt, faBraille, faLayerGroup, faArrowUp, faPlus, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { jsx, css } from '@emotion/core';
 
 export default function SideBar({ theme: [ columnBg, menuBgHover, activeItem, activeItemText, hoverItem, textColor, activePresence, mentionBadge ]}) {
   const channels = ['front-end-set-up','general', 'random'];
   const messages = ['Slackbot','yisselda', 'terrance', 'christine'];
-  const apps = ['Install Giphy', 'Install Simple Poll', 'Add more apps'];
   
   const hoverBackground = () => css({
     '&:hover': {
@@ -37,41 +36,111 @@ export default function SideBar({ theme: [ columnBg, menuBgHover, activeItem, ac
         backgroundColor: menuBgHover
       }
     }}>
-      <FontAwesomeIcon className='bell-icon' icon={['far', 'bell']} />
+      <FontAwesomeIcon className='edit-icon' icon={faEdit} />
       <h1 className='company-name'>SlackableThemes <FontAwesomeIcon className='down-icon' icon={faChevronDown} /> </h1>
       <p className='username'><span className='active-status' style={{ color: activePresence }} >●</span> Yisselda</p>
     </div>
 
     <div className='sidebar-nav'>
-      <input className='find-input' type='text' value='Jump to...' readOnly style={{ color: textColor }} />
+      {/* <div className='options-container'> */}
+        <table className='options-container'>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='chat-icon' icon={faCommentDots} /> 
+            </td>
+            <td>
+              <p className='threads-option' css={ hoverBackground }>Threads</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='at-icon' icon={faAt} />
+            </td>
+            <td>
+              <p className='mentions-option' css={ hoverBackground }>Mentions & reactions</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='draft-icon' icon={faCopy} />
+            </td>
+            <td>
+              <p className='drafts-option' css={ hoverBackground }>Drafts</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='bookmark-icon' icon={faBookmark} />
+            </td>
+            <td>
+              <p className='bookmarks-option' css={ hoverBackground }>Saved Items</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='people-icon' icon={faAddressCard} />
+            </td>
+            <td>
+              <p className='people-option' css={ hoverBackground }>People</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='apps-icon' icon={faBraille} />
+            </td>
+            <td>
+              <p className='apps-option' css={ hoverBackground }>Apps</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='files-icon' icon={faLayerGroup} />
+            </td>
+            <td>
+              <p className='files-option' css={ hoverBackground }>Files</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <FontAwesomeIcon className='up-icon' icon={faArrowUp} />
+            </td>
+            <td>
+              <p className='less-option' css={ hoverBackground }>Show Less</p>
+            </td>
+          </tr>
+        </table>
 
-      <p className='threads' css={ hoverBackground }><FontAwesomeIcon className='chat-icon' icon={faCommentDots} /> Threads</p>
-
-      <FontAwesomeIcon className='plus-icon' icon={faPlusCircle} />
-      <h2 className='channels' css={ hoverBoldText }>Channels</h2>
-      <ul className='channels-list' >
-        {channels.map((channel) => 
-          <li key={channel} css={ hoverBackground }>
-            <a  href='#' className={channel} css={ activeBackground }>
-              # <span className="channel-name">{channel}</span>
-              <span className="mention-badge" style={{ backgroundColor: mentionBadge }}>2</span>
-            </a>
-          </li>)}
-      </ul>
-
-      <FontAwesomeIcon className='plus-icon' icon={faPlusCircle} />
-      <h2 className='direct-messages' css={ hoverBoldText }>Direct Messages</h2>
-      <ul className='messages-list'>
-        {messages.map((message) => <li key={message} css={ hoverBackground }><a href='#' className={message} css={ activeBackground }><span className='active-status' style={{ color: activePresence }}>●</span> {message}</a></li>)}
-      </ul>
-
-      <p className='invite-people' css={ hoverBackground }>+ Invite people</p>
-
-      <FontAwesomeIcon className='plus-icon' icon={faPlusCircle} />
-      <h2 className='apps' css={ hoverBoldText }>Apps</h2>
-      <ul className='apps-list'>
-        {apps.map((app) => <li key={app} css={ hoverBackground }><a href='#'>+ {app}</a></li>)}
-      </ul>
+    <div className='chat-container'>
+      <div className='chat-options'>
+        <div className='channels-container'>
+        <FontAwesomeIcon className='down-icon' icon={faCaretDown} />
+        <FontAwesomeIcon className='plus-icon' icon={faPlus} />
+          <h2 className='channels' css={ hoverBoldText }>Channels</h2>
+          <ul className='channels-list' >
+            {channels.map((channel) => 
+              <li key={channel} css={ hoverBackground }>
+                <a  href='#' className={channel} css={ activeBackground }>
+                  # &nbsp;<span className="channel-name">{channel}</span>
+                  <span className="mention-badge" style={{ backgroundColor: mentionBadge }}>2</span>
+                </a>
+              </li>)}
+          </ul>
+        </div>
+            
+        <div className='messages-container'>
+        <FontAwesomeIcon className='down-icon' icon={faCaretDown} />
+        <FontAwesomeIcon className='plus-icon' icon={faPlus} />
+          <h2 className='direct-messages' css={ hoverBoldText }>Direct Messages</h2>
+          <ul className='messages-list'>
+            {messages.map((message) => <li key={message} css={ hoverBackground }><a href='#' className={message} css={ activeBackground }><span className='active-status' style={{ color: activePresence }}>● &nbsp;</span> {message}</a></li>)}
+          </ul>
+        </div>
+        
+        <FontAwesomeIcon className='down-icon' icon={faCaretDown} />
+        <FontAwesomeIcon className='plus-icon' icon={faPlus} />
+        <h2 className='apps' css={ hoverBoldText }>Apps</h2>
+      </div>
+    </div>
     </div>
   </div>
   )
