a1c7bcd2f3bc6da70b58db567db78abfd8217d99
Yisselda <yisselda@defmethod.io>
2020-04-28 11:42:55 -0400

    Preview Image

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -4,9 +4,12 @@ import '../styles/image-upload.scss';
 import ApiCommunicator from '../data/api-communicator';
 
 const ImageUpload = ({ updateThemeColors }) => {
+  const [file, setFile] = React.useState(null)
+
   const onDrop = async acceptedFiles => {
     let formData = new FormData();
     formData.append('file', acceptedFiles[0]);
+    setFile(acceptedFiles[0])
     const themeJSON = await ApiCommunicator.generateThemeFromFile(formData);
     const theme = themeJSON.data.split(",")
     updateThemeColors(theme);
@@ -15,11 +18,15 @@ const ImageUpload = ({ updateThemeColors }) => {
   return (
     <div className="image-upload-container">
       <div className="image-upload">
-        <Dropzone onDrop={onDrop}>
+        <Dropzone onDrop={onDrop} className="dropzone">
           {({getRootProps, getInputProps}) => (
-            <div {...getRootProps()}>
+            <div {...getRootProps()} className="image-input" >
               <input {...getInputProps()} />
-              Click me to upload a file!
+              {
+              file ?
+              <img src={URL.createObjectURL(file)} alt={file.name} />
+              : "Click me to upload a file!"
+              }
             </div>
           )}
         </Dropzone>
