88ee59f9b11398bad3ec79f04442e704d5197580
Kallista <kallista.rhoc@hotmail.com>
2020-05-01 21:48:23 +0200

    Copy button turns to Copied for 2 seconds after copying theme to clipboard

--- src/components/ColorInput.js
+++ src/components/ColorInput.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/color-input.scss';
 
 export default function ColorInput({ theme }){
   const copyToClipboard = (str, e) => {
     e.preventDefault();
     var promise = navigator.clipboard.writeText(str);
+    setCopied(true);
+
+    setTimeout(function(){setCopied(false)}, 2000);
   }
 
+  const [copied, setCopied] = useState(false);
   return (
     <div className='color-input'>
       <form>
         <input type="text" className="color-input-box" value={theme} readOnly/>
-        <input type="submit" className="copy-button" value="Copy" onClick={(e) => copyToClipboard(theme, e)} />
+        <input type="submit" className={copied ? "copy-button copied" : "copy-button"} value={copied ? "Copied !" : "Copy"} onClick={(e) => copyToClipboard(theme, e)} />
       </form>
     </div>
   )
