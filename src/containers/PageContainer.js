2b20bd890f287f3e1d86e0bccd169c5d4241c785
Punn Siriphanthong <p.siriphanthong@gmail.com>
2020-10-26 06:14:12 +0700

    add GithubCorner into page

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -1,29 +1,34 @@
 import React, { useState } from 'react'
-import SideBar from '../components/SideBar';
-import DashBoard from '../components/DashBoard';
-import presetThemes from '../data/themes';
-import '../styles/page-container.scss';
-import PropTypes from 'prop-types';
+import SideBar from '../components/SideBar'
+import DashBoard from '../components/DashBoard'
+import presetThemes from '../data/themes'
+import '../styles/page-container.scss'
+import PropTypes from 'prop-types'
+import GithubCorner from 'react-github-corner'
 
-export default function PageContainer(){
-  const [theme, setTheme] = useState(presetThemes[0].themeColor);
+export default function PageContainer() {
+  const [theme, setTheme] = useState(presetThemes[0].themeColor)
 
   const updateTheme = (t) => {
-    setTheme([...t]);
-  };
+    setTheme([...t])
+  }
 
   return (
     <div className='page-container'>
       <SideBar theme={theme} />
-      <DashBoard setTheme={updateTheme} theme={theme}/>
+      <DashBoard setTheme={updateTheme} theme={theme} />
+      <GithubCorner
+        href='https://github.com/yisselda/SlackableThemes'
+        target='__blank'
+      />
     </div>
   )
-};
+}
 
 PageContainer.defaultProps = {
   theme: presetThemes[0].themeColor,
-};
+}
 
 PageContainer.propTypes = {
   theme: PropTypes.arrayOf(PropTypes.string.isRequired),
-};
+}
