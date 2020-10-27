49785e2f28d39cd9c8c1bb689b77a54f00039a06
Steve O'Keefe <NLSteveO@users.noreply.github.com>
2020-10-27 01:13:20 -0230

    Fix #82 Storybook is broken (#111)
    Storybook wouldn't work because the presetThemes weren't being passed into Dashboard and Sidebar which those files expect. Then once i made that change the images for sidebar themes wouldn't load but i found the solution on the storybook docs, you need to pass in the directory where your assets live so i edited the npm script for storybook to include -s ./public

--- src/stories/Dashboard.stories.js
+++ src/stories/Dashboard.stories.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import presetThemes from '../data/themes';
 import Dashboard from "../components/DashBoard";
 
 export default { title: "Dashboard" };
 
-export const Default = () => <Dashboard />;
+export const Default = () => {
+  const [theme, setTheme] = useState(presetThemes[0].themeColor);
+
+  const updateTheme = (t) => {
+    setTheme([...t]);
+  };
+
+  return <Dashboard setTheme={updateTheme} theme={theme}/>
+};
